0d378936a60ad63ea9c600145fb891480bb97402
Zhansaya Atlas <zhansayaatlas@gmail.com>
2026-04-09 10:52:29 +0500

    Update task_2_python.py

--- task_python_2/task_2_python.py
+++ task_python_2/task_2_python.py
@@ -5,28 +5,12 @@
 # TASK4
 # words = input().split()
 # print(max(words, key=len))
-#  TASK4
-# s = input()
-# best = cur = s[0]
-# for i in range(1, len(s)):
-#     if s[i] > s[i-1]:
-#         cur += s[i]
-#     else:
-#         if len(cur) > len(best):
-#             best = cur
-#         cur = s[i]
-# if len(cur) > len(best):
-#     best = cur
-# print(best)
+# TASK2
+# 
 # TASK3
-# words = input().split()
-# freq = {}
-# for word in words:
-#     if word not in freq:
-#         freq[word] = 0
-#     freq[word] += 1
-# for word in freq:
-#     print(word + ": " + str(freq[word]))
+# s = input().split()
+# for word in dict.fromkeys(s):
+# print(f"{word}: {s.count(word)}")
 # TASK5
 # s = input()
 # digits = ""
